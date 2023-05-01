e839549ab2e7ccf85cf2203a30aaa92a7ec1f3b2
YayCover9000 <yassuanf@googlemail.com>
2023-05-01 15:44:45 +0200

    add array[] but can not access it

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,22 @@
 public class Main {
     //Commit der Frage
     public static void main(String[] args) {
-/*
-        Money income = new Money(100, "EUR");
-        Money expenses = new Money(100, "EUR");
-        boolean balance = income.equals(expenses);
-        System.out.println("Balance: " + balance);*/
-
-        Money eineZahl = new Money("jp", "ep", 4,100, "EUR");
-        System.out.println(eineZahl);
-
-
-
-       Professor prof = new Professor("John", "Dummy", 20, 1000, "EUR", "Informatik", 100, "EUR");
-        System.out.println(prof.toString());
-
+        // TODO Auto-generated method stub
+        Professor prof = new Professor("John", "Dummy", 20, 1000, "EUR", "Informatik", 100, "EUR");
         Professor profJapanisch = new Professor("Herman", "Konichiwa", 26, 10, "YEN", "Japanisch", 1000, "YEN");
-        System.out.println(profJapanisch.toString());
+        Student Lauri =  new Student("Lauri", "Love", 24, 10000, "EUR", "Umweltinformatik", 1234554321);
+        Student yassuan = new Student("Yassuan", "Hallo", 25, 10, "EUR", "Medizininformatik", 87667890);
+        System.out.println(yassuan.equals(Lauri));
 
+
+
+    printPerson(Lauri);
+    printPerson(prof);
+    printPerson(profJapanisch);
+    printPerson(yassuan);
+
+    }
+    public static void printPerson(Person person) {
+        System.out.println("Person: " + person.toString());
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,49 @@
 public class Student extends Money {
     private String studiengang;
     private int matrikelnummer;
+    private Student[] studenten;
+    private int index;
+    private final static int MAX_STUDENTEN = 10;
 
     public Student (String firstname, String lastname, int age, int amount, String currency, String studiengang, int matrikelnummer){
         super(firstname, lastname, age, amount, currency);
         this.studiengang = studiengang;
         this.matrikelnummer = matrikelnummer;
+        studenten = new Student[MAX_STUDENTEN];
+        index = 0;
+
     }
     public Student() {
         this(null, null, 0, 0, null, null, 0);
     }
+    //Objektverwaltung
+    public void addStudent (Student student) {
+        if (studenten != null) {
+            if (index < MAX_STUDENTEN) {
+                studenten[index] = student;
+                index++;
+            }
+        }
+    }
+    public void deleteStudent () {
+        if (studenten != null) {
+            if (index > 0) {
+                studenten[index - 1] = null;
+                index--;
+            } else if (index == 0) {
+                studenten[index] = null;
+            }
+        }
+    }
+    public Student getStudent (int index) {
+        if (studenten != null) {
+            if (index >= 0 && index < MAX_STUDENTEN) {
+                return studenten[index];
+            }
+        }
+        return null;
+    }
+
     //Java Standard Methode
     @Override
     public Person clone() {
@@ -30,7 +64,7 @@ public class Student extends Money {
         if (o == null) return false;
         if (this.getClass() != o.getClass()) return false;
         Student otherCastStudent = (Student) o;
-        return super.equals(o) + this.studiengang.equals(o.studiengang) + this.matrikelnummer.equals(o.matrikelnummer);
+        return super.equals(o) && this.studiengang.equals(otherCastStudent.studiengang) && (this.matrikelnummer == otherCastStudent.matrikelnummer);
     }
 
     public String getStudiengang() {
@@ -48,4 +82,14 @@ public class Student extends Money {
     public void setMatrikelnummer(int matrikelnummer) {
         this.matrikelnummer = matrikelnummer;
     }
+    public Student[] getStudenten() {
+        return studenten;
+    }
+    public int getIndex() {
+        return index;
+    }
+    public int getNumberOfStudents() {
+        return index;
+    }
+
 }
