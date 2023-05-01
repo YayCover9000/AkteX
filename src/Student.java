6584e61e8d28fc20a18eeddde40f68baf4fba091
YayCover9000 <yassuanf@googlemail.com>
2023-05-01 12:39:19 +0200

    add methodes

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String studiengang;
+    private int matrikelnummer;
+
+    public Student (String studiengang, int matrikelnummer){
+        this.studiengang = studiengang;
+        this.matrikelnummer = matrikelnummer;
+    }
+}
